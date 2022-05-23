7f828a6eeae5a35012311fbdd0ab573c020ec0e4
farrelleoin93 <farrelleoin93@gmail.com>
2022-05-23 09:40:29 +0100

    Added unfinished projects.js file

---
+++ assets/js/projects.js
@@ -0,0 +1,29 @@
+let projects = [
+  {
+    title: "12 Acres Beer",
+    subtitle: "E-commerce website",
+    description:
+      "This is an e-commerce website that I built for a local craft beer company in my area. It was build using Django. Users can buy beer using fake Stripe details.",
+    skills: [
+      "html5",
+      "css3",
+      "javascript",
+      "python",
+      "django",
+      "bootstrap",
+      "heroku",
+    ],
+    repo: "https://github.com/farrelleoin93/12-acres-beer",
+    site: "https://farrelleoin93-12-acres.herokuapp.com/",
+    image: "./assets/images/projects/12-acres-devices.png", 
+    alt: "The 12 Acres responsive image"
+  },
+];
+
+const html = '';
+
+// const skills = projects.skills.forEach(skill => )
+
+projects.map(project => {
+    html +=  
+})
